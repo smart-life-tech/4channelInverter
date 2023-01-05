1be1acb33748f07a6fecdf364e8e00a3a68520fe
smart-life-tech <christlightworld@gmail.com>
2023-01-05 14:18:40 +0100

    modified:   inverter1/invterter1.ino

--- inverter1/invterter1.ino
+++ inverter1/invterter1.ino
@@ -1,7 +1,7 @@
 #define F_CPU 16000000        // Set the clock frequency
 #define PORTB_MASK 0b00001111 // Mask to set only the lower four bits of PORTB
 #define PORTD_MASK 0b11110000 // Mask to set only the upper four bits of PORTD
-int period = 33.4;
+int period =8325;// 60 hz perod
 #include <avr/io.h>
 #include <util/delay.h>
 
